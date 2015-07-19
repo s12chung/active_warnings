c99dcf197128a6727d5cd82308db98ef29580ecc
Steve Chung <steve.chung7@gmail.com>
2015-07-18 21:51:46 -0400

    complex version

--- lib/active_warnings.rb
+++ lib/active_warnings.rb
@@ -1,5 +1,59 @@
 require "active_warnings/version"
 
+require 'active_model'
+require 'active_support/all'
+
 module ActiveWarnings
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  included do
+    include ActiveModel::Validations
+
+    define_callbacks :validate_warning
+
+    def errors
+      @run_warning_validations ? warnings : super
+    end
+
+    protected
+    # Override and change one line:
+    # https://github.com/rails/rails/blob/64c1264419f766a306eba0418c1030b87489ea67/activemodel/lib/active_model/validations.rb#L406
+    def run_validations!
+      super unless @run_warning_validations
+      run_callbacks :validate_warning
+      errors.empty?
+    end
+
+    class << self
+      def warnings
+        @within_warnings = true
+        yield
+      ensure
+        @within_warnings = nil
+      end
+
+      # Change first parameter of:
+      # https://github.com/rails/rails/blob/beb07fbfae845d20323a9863c7216c6b63aff9c7/activemodel/lib/active_model/validations.rb#L170
+      def set_callback(name, *filter_list, &block)
+        return super unless name == :validate && @within_warnings
+        super(:validate_warning, *filter_list, &block)
+      end
+    end
+  end
+
+  protected
+  def warnings
+    @warnings ||= Errors.new(self)
+  end
+
+  def has_warnings?(context=nil)
+    @run_warning_validations = true
+    valid?(context)
+  ensure
+    @run_warning_validations = nil
+  end
+
+  def no_warnings?(context=nil)
+    !has_warnings?(context)
+  end
 end
--- spec/active_warnings_spec.rb
+++ spec/active_warnings_spec.rb
@@ -4,4 +4,107 @@ describe ActiveWarnings do
   it 'has a version number' do
     expect(ActiveWarnings::VERSION).not_to be nil
   end
+
+  context "instance" do
+    class BasicModel
+      include ActiveWarnings
+
+      attr_accessor :name, :warning_name
+
+      validates :name, presence: true
+
+      warnings do
+        validates :warning_name, presence: true
+      end
+    end
+
+    let(:instance) { BasicModel.new }
+
+    describe "#run_validations!" do
+      subject { instance.send :run_validations! }
+
+      it "only runs the warning validations" do
+        subject
+        expect(instance.errors.keys).to eql :warning_name
+      end
+    end
+  end
+
+  context "class" do
+    describe "::warnings" do
+      let(:klass) do
+        Class.new do
+          include ActiveWarnings
+
+          def self.raise_error
+            return unless @within_warnings
+            raise "true"
+          end
+
+          warnings do
+            raise_error
+          end
+        end
+      end
+
+      subject { klass }
+
+      it "makes @within_warnings true" do
+        expect { subject }.to raise_error
+      end
+    end
+
+    describe "::set_callback" do
+      let(:klass) { Class.new { include ActiveWarnings } }
+
+      def callback_chain_size(name)
+        klass.send(:get_callbacks, name).send(:chain).size
+      end
+
+      context "when setting non-validate callback" do
+        let(:options) { {} }
+
+        subject do
+          klass.define_callbacks :save
+          klass.set_callback(:save, options) { }
+        end
+
+        it "adds the callback to save" do
+          subject
+          expect(callback_chain_size(:save)).to eql 1
+          expect(callback_chain_size(:validate_warning)).to eql 0
+        end
+      end
+
+      context "when setting a validate callback" do
+        subject do
+          klass.validates(:name, presence: true)
+        end
+
+        it "adds the callback to save to validate" do
+          subject
+          expect(callback_chain_size(:validate)).to eql 1
+          expect(callback_chain_size(:validate_warning)).to eql 0
+        end
+
+        context "within warnings block" do
+          let(:klass) do
+            Class.new do
+              include ActiveWarnings
+              warnings do
+                validates(:name, presence: true)
+              end
+            end
+          end
+
+          subject { klass }
+
+          it "adds the callback to save to validate_warning" do
+            subject
+            expect(callback_chain_size(:validate_warning)).to eql 1
+          end
+        end
+      end
+    end
+  end
 end
